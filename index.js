64488eec804f47a18434fce60604132f6ac8d129
Erik Mende <emende@student.hive.fi>
2022-09-05 01:41:24 +0300

    Exercises 3.2.-3.6. is done

--- index.js
+++ index.js
@@ -27,11 +27,58 @@ let notes = [
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+	res.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/persons', (req, res) => {
-  res.json(notes)
+	res.json(notes)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+	const id = Number(req.params.id)
+	const note = notes.find(note => note.id === id)
+	if (note) {
+		res.json(note)
+	} else {
+		res.status(404).end()
+	}
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+	const id = Number(req.params.id)
+	notes = notes.filter(note => note.id !== id)
+
+	res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+	const body = req.body
+
+	if (!body.name || !body.number) {
+		return res.status(400).json({
+			error: 'content missing'
+		})
+	}
+
+	if (notes.find(note => note.name === body.name)) {
+		return res.status(400).json({
+			error: 'name must be unique'
+		})
+	}
+
+	const note = {
+		name: body.name,
+		number: body.number,
+		id: Math.floor(Math.random() * 13371337),
+	}
+
+	notes = notes.concat(note)
+
+	res.json(note)
+})
+
+app.get('/info', (req, res) => {
+	res.send(`<p>Phonebook has info for ${notes.length} people</p> <p>${Date()}</p>`)
 })
 
 const PORT = 3001
