4b49baa646b9c79199a51a3b5ca07ebf4981884d
Erik Mende <emende@student.hive.fi>
2022-09-05 02:09:18 +0300

    Exercises 3.7.-3.8. morgon logging exercises

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+var morgan = require('morgan')
 const app = express()
 
 let notes = [
@@ -25,6 +26,17 @@ let notes = [
 ]
 
 app.use(express.json())
+
+app.use(morgan(function (tokens, req, res) {
+	return [
+	  tokens.method(req, res),
+	  tokens.url(req, res),
+	  tokens.status(req, res),
+	  tokens.res(req, res, 'content-length'), '-',
+	  tokens['response-time'](req, res), 'ms',
+	  JSON.stringify(req.body)
+	].join(' ')
+}))
 
 app.get('/', (req, res) => {
 	res.send('<h1>Hello World!</h1>')
