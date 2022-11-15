9aeb42469ed90fcaf5875200c024f4b68cb762e1
Erik Mende <emende@student.hive.fi>
2022-11-15 23:00:50 +0200

    Exercises 3.13.-3.18. done

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
+require("dotenv").config();
 const express = require("express");
-var morgan = require("morgan");
 const app = express();
+const Person = require("./models/person");
+const mongoose = require("mongoose");
+var morgan = require("morgan");
 const cors = require("cors");
 app.use(cors());
 app.use(express.static("build"));
-
-let notes = [
-	{
-		id: 1,
-		name: "Arto Hellas",
-		number: "040-123456",
-	},
-	{
-		id: 2,
-		name: "Ada Lovelace",
-		number: "39-44-5323523",
-	},
-	{
-		id: 3,
-		name: "Dan Abramov",
-		number: "12-43-234345",
-	},
-	{
-		id: 4,
-		name: "Mary Poppendieck",
-		number: "39-23-6423122",
-	},
-];
-
 app.use(express.json());
 
 app.use(
@@ -50,71 +29,100 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/persons", (req, res) => {
-	res.json(notes);
+	Person.find({}).then((notes) => {
+		res.json(notes);
+	});
 });
 
-app.get("/api/persons/:id", (req, res) => {
-	const id = Number(req.params.id);
-	const note = notes.find((note) => note.id === id);
-	if (note) {
-		res.json(note);
-	} else {
-		res.status(404).end();
-	}
+app.get("/api/persons/:id", (req, res, next) => {
+	Person.findById(req.params.id)
+		.then((note) => {
+			if (note) {
+				res.json(note);
+			} else {
+				res.status(404).end();
+			}
+		})
+		.catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-	const id = Number(req.params.id);
-	notes = notes.filter((note) => note.id !== id);
-
-	res.status(204).end();
+app.delete("/api/persons/:id", (req, res, next) => {
+	Person.findByIdAndRemove(req.params.id)
+		.then((result) => {
+			res.status(204).end();
+		})
+		.catch((error) => next(error));
 });
 
 app.post("/api/persons", (req, res) => {
-	const body = req.body;
+	const { name, number } = req.body;
 
-	if (!body.name || !body.number) {
-		return res.status(400).json({
-			error: "content missing",
-		});
-	}
+	if (!name || !number)
+		return res.status(400).json({ error: "content missing" });
 
-	if (notes.find((note) => note.name === body.name)) {
-		return res.status(400).json({
-			error: "name must be unique",
-		});
-	}
+	Person.findOne({ name }).then((person) => {
+		if (person) {
+			return res.status(400).json({ error: "name must be unique" });
+		}
+	});
 
-	const note = {
-		name: body.name,
-		number: body.number,
-		id: Math.floor(Math.random() * 13371337),
+	const person = new Person({
+		name,
+		number,
+	});
+
+	person.save().then((savedPerson) => {
+		res.json(savedPerson);
+	});
+});
+
+app.put("/api/persons/:id", (req, res, next) => {
+	const { name, number } = req.body;
+	const id = req.params.id;
+
+	if (!name || !number || !id)
+		return res.status(400).json({ error: "content missing" });
+	const person = {
+		name,
+		number,
 	};
 
-	notes = notes.concat(note);
-
-	res.json(note);
+	Person.findByIdAndUpdate(id, person, { new: true })
+		.then((updatedPerson) => {
+			res.json(updatedPerson);
+		})
+		.catch((error) => next(error));
 });
 
 app.get("/info", (req, res) => {
-	res.send(
-		`<p>Phonebook has info for ${notes.length} people</p> <p>${Date()}</p>`
-	);
+	Person.find({}).then((notes) => {
+		res.send(
+			`<p>Phonebook has info for ${
+				notes.length
+			} people</p> <p>${Date()}</p>`
+		);
+	});
 });
 
-app.put("/api/persons/:id", (req, res) => {
-	const id = Number(req.params.id);
-	const { name, number } = req.body;
+const unknownEndpoint = (request, response) => {
+	response.status(404).send({ error: "unknown endpoint" });
+};
 
-	const note = {
-		name,
-		number,
-		id,
-	};
-	notes = notes.map((note) => (note.id !== id ? note : note));
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint);
 
-	res.json(note);
-});
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message);
+
+	if (error.name === "CastError") {
+		return response.status(400).send({ error: "malformatted id" });
+	}
+
+	next(error);
+};
+
+// this has to be the last loaded middleware.
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
